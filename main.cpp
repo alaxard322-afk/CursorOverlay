083dd4518e3fa788f6bed38e7b94a4b441497a82
alaxard322-afk <alaxard322@gmail.com>
2026-07-09 16:35:06 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    MessageBoxA(
+        NULL,
+        "CursorOverlay test",
+        "CursorOverlay",
+        MB_OK
+    );
+
+    return 0;
+}
